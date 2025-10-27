fb9b8f2b4b0687768b1aa221930a1163d5994385
kylo <huiboxes@gmail.com>
2025-10-27 23:13:25 +0800

    feat: 贪心解决，要么继续之前的子数组要么重新开始

--- leetcode/editor/cn/MaximumSubarray.java
+++ leetcode/editor/cn/MaximumSubarray.java
@@ -55,19 +55,16 @@ public class MaximumSubarray {
     //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int maxSubArray(int[] nums) {
+        int max = nums[0];
+        int sum = nums[0];
 
-        int maxSum = nums[0];
-        int minPrefixSum = 0;
-        int prefixSum = 0;
-
-        for (int i = 0; i < nums.length; i++) {
-            prefixSum += nums[i];
-            maxSum = Math.max(maxSum, prefixSum - minPrefixSum);
-            minPrefixSum = Math.min(minPrefixSum, prefixSum);
+        for (int i = 1; i < nums.length; i++) {
+            sum = Math.max(sum + nums[i], nums[i]);
+            max = Math.max(max, sum);
         }
 
 
-        return maxSum;
+        return max;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
