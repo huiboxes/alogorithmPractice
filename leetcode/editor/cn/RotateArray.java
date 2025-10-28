d5daf0c1f7a431b7ed927f4296b68e20b46f4cc4
kylo <huiboxes@gmail.com>
2025-10-29 00:00:24 +0800

    feat: 三次反转数组

--- leetcode/editor/cn/RotateArray.java
+++ leetcode/editor/cn/RotateArray.java
@@ -62,23 +62,22 @@ class Solution {
     public void rotate(int[] nums, int k) {
         if(nums.length <= 1) return;
         k = k % nums.length;
-        int count = 0;
 
-        for (int i = 0; count < nums.length; i++) {
-            int current = i;
-            int pre = nums[i];
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
 
-            do {
-                int next = (current + k) % nums.length;
-                int temp = nums[next];
-                nums[next] = pre;
-                pre = temp;
-                current = next;
-                count++;
-            } while (i != current);
 
+    }
+
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
         }
-
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
