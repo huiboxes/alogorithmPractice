5f4f4e9eba5d9902e3dc018d1d880ba1ebfa357b
kylo <huiboxes@gmail.com>
2025-10-29 23:36:11 +0800

    feat: 双指针，保存当前节点和上一个节点，将当前节点设置为下一个节点，当前节点的next等于上有个节点

--- leetcode/editor/cn/ReverseLinkedList.java
+++ leetcode/editor/cn/ReverseLinkedList.java
@@ -81,22 +81,17 @@ class Solution {
     public ListNode reverseList(ListNode head) {
         if (head == null || head.next == null) return head;
 
-        Stack<ListNode> listNodes = new Stack<>();
-        while (head != null) {
-            listNodes.push(head);
-            head = head.next;
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+            ListNode temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
         }
 
-        ListNode newHead = listNodes.pop();
-        ListNode current = newHead;
-        while (!listNodes.isEmpty()) {
-            current.next = listNodes.pop();
-            current = current.next;
-        }
-
-        current.next = null;
-
-        return newHead;
+        return prev;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
