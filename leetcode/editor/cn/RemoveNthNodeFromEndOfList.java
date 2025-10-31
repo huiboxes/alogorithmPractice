a2029e5a510cdc95236d0f730f086d2a01396119
kylo <huiboxes@gmail.com>
2025-10-31 20:00:32 +0800

    feat: 快慢指针，快指针先走n步，然后快慢指针同时走，快指针走到头时慢指针就是要删除的节点

---
+++ leetcode/editor/cn/RemoveNthNodeFromEndOfList.java
@@ -0,0 +1,99 @@
+/**
+  * 题目Id：19
+  * 题目：删除链表的倒数第 N 个结点
+  * 日期：2025-10-31 19:46:33
+*/
+//给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。 
+//
+// 
+//
+// 示例 1： 
+// 
+// 
+//输入：head = [1,2,3,4,5], n = 2
+//输出：[1,2,3,5]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：head = [1], n = 1
+//输出：[]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：head = [1,2], n = 1
+//输出：[1]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 链表中结点的数目为 sz 
+// 1 <= sz <= 30 
+// 0 <= Node.val <= 100 
+// 1 <= n <= sz 
+// 
+//
+// 
+//
+// 进阶：你能尝试使用一趟扫描实现吗？ 
+//
+// Related Topics 链表 双指针 👍 3201 👎 0
+
+package editor.cn;
+
+import java.util.ArrayList;
+
+public class RemoveNthNodeFromEndOfList {
+    public static void main(String[] args) {
+        Solution solution = new RemoveNthNodeFromEndOfList().new Solution();
+        System.out.println("Hello world");
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+
+//    public static class ListNode {
+//        int val;
+//        ListNode next;
+//        ListNode() {}
+//        ListNode(int val) { this.val = val; }
+//        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+//    }
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode slow = dummy;
+        ListNode fast = dummy;
+        for (int i = 0; i <= n; i++) {
+            fast = fast.next;
+        }
+
+        while (fast != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        slow.next = slow.next.next;
+
+        return dummy.next;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+} 
