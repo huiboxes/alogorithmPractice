f48edeaf90fcf007150b282ee9b42cc75330248b
kylo <huiboxes@gmail.com>
2025-11-04 23:11:59 +0800

    feat: 遍历输入的链表，通过mergeTwoList逐个合并

---
+++ leetcode/editor/cn/MergeKSortedLists.java
@@ -0,0 +1,114 @@
+/**
+  * 题目Id：23
+  * 题目：合并 K 个升序链表
+  * 日期：2025-11-04 23:02:58
+*/
+//给你一个链表数组，每个链表都已经按升序排列。 
+//
+// 请你将所有链表合并到一个升序链表中，返回合并后的链表。 
+//
+// 
+//
+// 示例 1： 
+//
+// 输入：lists = [[1,4,5],[1,3,4],[2,6]]
+//输出：[1,1,2,3,4,4,5,6]
+//解释：链表数组如下：
+//[
+//  1->4->5,
+//  1->3->4,
+//  2->6
+//]
+//将它们合并到一个有序链表中得到。
+//1->1->2->3->4->4->5->6
+// 
+//
+// 示例 2： 
+//
+// 输入：lists = []
+//输出：[]
+// 
+//
+// 示例 3： 
+//
+// 输入：lists = [[]]
+//输出：[]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// k == lists.length 
+// 0 <= k <= 10^4 
+// 0 <= lists[i].length <= 500 
+// -10^4 <= lists[i][j] <= 10^4 
+// lists[i] 按 升序 排列 
+// lists[i].length 的总和不超过 10^4 
+// 
+//
+// Related Topics 链表 分治 堆（优先队列） 归并排序 👍 3091 👎 0
+
+package editor.cn;
+public class MergeKSortedLists {
+    public static void main(String[] args) {
+        Solution solution = new MergeKSortedLists().new Solution();
+        System.out.println("Hello world");
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+
+//    public class ListNode {
+//        int val;
+//        ListNode next;
+//        ListNode() {}
+//        ListNode(int val) { this.val = val; }
+//        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+//    }
+
+    public ListNode mergeKLists(ListNode[] lists) {
+        if(lists == null || lists.length == 0) {
+            return null;
+        }
+
+        ListNode res = null;
+        for (ListNode list : lists) {
+            res = mergerTwoLists(res, list);
+        }
+        return res;
+    }
+
+    private ListNode mergerTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+
+        while (l1 != null && l2 != null) {
+            if(l1.val <= l2.val) {
+                current.next = l1;
+                l1 = l1.next;
+            } else {
+                current.next = l2;
+                l2 = l2.next;
+            }
+            current = current.next;
+        }
+
+        current.next = l1 == null ? l2 : l1;
+        return dummy.next;
+    }
+
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+} 
