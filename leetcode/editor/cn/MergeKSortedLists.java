cbbdf2f6d9a7203c6c6479fa9f5047eb0e690112
kylo <huiboxes@gmail.com>
2025-11-04 23:26:20 +0800

    feat: 分治解决，递归二分合并

--- leetcode/editor/cn/MergeKSortedLists.java
+++ leetcode/editor/cn/MergeKSortedLists.java
@@ -82,30 +82,29 @@ class Solution {
             return null;
         }
 
-        ListNode res = null;
-        for (ListNode list : lists) {
-            res = mergerTwoLists(res, list);
-        }
-        return res;
+        return merge(lists, 0, lists.length - 1);
+    }
+
+    private ListNode merge(ListNode[] lists, int left, int right) {
+        if(left == right) return lists[left];
+        if(left > right) return null;
+
+        int mid = left + (right - left) / 2;
+        ListNode l1 = merge(lists, left, mid);
+        ListNode l2 = merge(lists, mid + 1, right);
+        return mergerTwoLists(l1, l2);
     }
 
     private ListNode mergerTwoLists(ListNode l1, ListNode l2) {
-        ListNode dummy = new ListNode(0);
-        ListNode current = dummy;
-
-        while (l1 != null && l2 != null) {
-            if(l1.val <= l2.val) {
-                current.next = l1;
-                l1 = l1.next;
-            } else {
-                current.next = l2;
-                l2 = l2.next;
-            }
-            current = current.next;
+        if(l1 == null) return l2;
+        if(l2 == null) return l1;
+        if(l1.val < l2.val) {
+            l1.next = mergerTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergerTwoLists(l1, l2.next);
+            return l2;
         }
-
-        current.next = l1 == null ? l2 : l1;
-        return dummy.next;
     }
 
 }
