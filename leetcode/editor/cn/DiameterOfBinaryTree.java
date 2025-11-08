6507ada56d38603221460b2a74e1d5db958dc8a8
kylo <huiboxes@gmail.com>
2025-11-09 01:18:10 +0800

    feat: 543-DFS。递归遍历二叉树，计算每个节点深度的同时利用左子树深度加右子树深度更新全局最大值。

---
+++ leetcode/editor/cn/DiameterOfBinaryTree.java
@@ -0,0 +1,89 @@
+/**
+  * 题目Id：543
+  * 题目：二叉树的直径
+  * 日期：2025-11-09 00:57:28
+*/
+//给你一棵二叉树的根节点，返回该树的 直径 。 
+//
+// 二叉树的 直径 是指树中任意两个节点之间最长路径的 长度 。这条路径可能经过也可能不经过根节点 root 。 
+//
+// 两节点之间路径的 长度 由它们之间边数表示。 
+//
+// 
+//
+// 示例 1： 
+// 
+// 
+//输入：root = [1,2,3,4,5]
+//输出：3
+//解释：3 ，取路径 [4,2,1,3] 或 [5,2,1,3] 的长度。
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：root = [1,2]
+//输出：1
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中节点数目在范围 [1, 10⁴] 内 
+// -100 <= Node.val <= 100 
+// 
+//
+// Related Topics 树 深度优先搜索 二叉树 👍 1818 👎 0
+
+package editor.cn;
+
+import javax.swing.tree.TreeNode;
+
+public class DiameterOfBinaryTree {
+    public static void main(String[] args) {
+        Solution solution = new DiameterOfBinaryTree().new Solution();
+        System.out.println("Hello world");
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    private int maxDiameter = 0;
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        maxDepth(root);
+        return maxDiameter;
+    }
+
+    private int maxDepth(TreeNode node) {
+        if(node == null) {
+            return 0;
+        }
+
+        int leftDepth = maxDepth(node.left);
+        int rightDepth = maxDepth(node.right);
+
+        maxDiameter = Math.max(maxDiameter, leftDepth + rightDepth);
+
+        return Math.max(leftDepth, rightDepth) + 1;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+} 
