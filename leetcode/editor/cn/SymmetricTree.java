fccdb1923a28ce9bfb773584c180f2eaba9425c2
kylo <huiboxes@gmail.com>
2025-11-09 00:57:04 +0800

    feat: 101-DFS。递归判断左右子树是否互为镜像。

--- leetcode/editor/cn/SymmetricTree.java
+++ leetcode/editor/cn/SymmetricTree.java
@@ -66,31 +66,23 @@ class Solution {
     public boolean isSymmetric(TreeNode root) {
         if(root == null) return true;
 
-        LinkedList<TreeNode> queue = new LinkedList<>();
-        queue.offer(root.left);
-        queue.offer(root.right);
 
-        while(!queue.isEmpty()) {
-            TreeNode leftNode = queue.poll();
-            TreeNode rightNode = queue.poll();
 
-            if(leftNode == null && rightNode == null) {
-                continue;
-            }
+        return isMirrot(root.left, root.right);
+    }
 
-            if(leftNode == null || rightNode == null || leftNode.val != rightNode.val) {
-                return false;
-            }
-
-            queue.offer(leftNode.left);
-            queue.offer(rightNode.right);
-
-            queue.offer(leftNode.right);
-            queue.offer(rightNode.left);
+    private boolean isMirrot(TreeNode left, TreeNode right) {
+        if(left == null && right == null) {
+            return true;
         }
 
-        return true;
+        if(left == null || right == null) {
+            return false;
+        }
 
+        return (left.val == right.val)
+                && isMirrot(left.left, right.right)
+                && isMirrot(left.right, right.left);
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
