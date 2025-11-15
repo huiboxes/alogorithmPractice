a053449090d238938ef64f9ef287dd99cf624de0
kylo <huiboxes@gmail.com>
2025-11-15 22:36:20 +0800

    feat: 199-BFS。列用队列逐层遍历，每次处理当前层的所有节点，只把当前层的最后一个节点添加到结果数组中。

---
+++ leetcode/editor/cn/BinaryTreeRightSideView.java
@@ -0,0 +1,124 @@
+/**
+  * 题目Id：199
+  * 题目：二叉树的右视图
+  * 日期：2025-11-15 22:13:29
+*/
+//给定一个二叉树的 根节点 root，想象自己站在它的右侧，按照从顶部到底部的顺序，返回从右侧所能看到的节点值。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+// 输入：root = [1,2,3,null,5,null,4] 
+// 
+//
+// 输出：[1,3,4] 
+//
+// 解释： 
+//
+// 
+//
+// 示例 2： 
+//
+// 
+// 输入：root = [1,2,3,4,null,null,null,5] 
+// 
+//
+// 输出：[1,3,4,5] 
+//
+// 解释： 
+//
+// 
+//
+// 示例 3： 
+//
+// 
+// 输入：root = [1,null,3] 
+// 
+//
+// 输出：[1,3] 
+//
+// 示例 4： 
+//
+// 
+// 输入：root = [] 
+// 
+//
+// 输出：[] 
+//
+// 
+//
+// 提示: 
+//
+// 
+// 二叉树的节点个数的范围是 [0,100] 
+// 
+// -100 <= Node.val <= 100 
+// 
+//
+// Related Topics 树 深度优先搜索 广度优先搜索 二叉树 👍 1256 👎 0
+
+package editor.cn;
+
+import javax.swing.tree.TreeNode;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class BinaryTreeRightSideView {
+    public static void main(String[] args) {
+        Solution solution = new BinaryTreeRightSideView().new Solution();
+        System.out.println("Hello world");
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        ArrayList<Integer> result = new ArrayList<>();
+
+        if(root == null) return result;
+
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while(!queue.isEmpty()) {
+            int levelSize = queue.size();
+
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode node = queue.poll();
+
+                if(i == levelSize - 1) {
+                    result.add(node.val);
+                }
+
+                if(node.left != null) queue.offer(node.left);
+
+                if(node.right != null) {
+                    queue.offer(node.right);
+                }
+
+            }
+        }
+
+        return result;
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+} 
