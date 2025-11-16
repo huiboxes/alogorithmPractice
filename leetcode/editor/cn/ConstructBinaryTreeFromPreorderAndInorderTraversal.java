0629d419964fb3bd1ffb3b36c8252247fc61c5f4
kylo <huiboxes@gmail.com>
2025-11-16 22:11:02 +0800

    feat: 105-递归。在前序遍历的结果中取出元素作为根节点，在中序遍历中找到根节点的位置。最后递归构建左子树[start,rootIndex-1]，右子树[rootIndex+1,end]。

---
+++ leetcode/editor/cn/ConstructBinaryTreeFromPreorderAndInorderTraversal.java
@@ -0,0 +1,97 @@
+/**
+  * 题目Id：105
+  * 题目：从前序与中序遍历序列构造二叉树
+  * 日期：2025-11-16 21:57:23
+*/
+//给定两个整数数组 preorder 和 inorder ，其中 preorder 是二叉树的先序遍历， inorder 是同一棵树的中序遍历，请构造二叉树并
+//返回其根节点。 
+//
+// 
+//
+// 示例 1: 
+// 
+// 
+//输入: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
+//输出: [3,9,20,null,null,15,7]
+// 
+//
+// 示例 2: 
+//
+// 
+//输入: preorder = [-1], inorder = [-1]
+//输出: [-1]
+// 
+//
+// 
+//
+// 提示: 
+//
+// 
+// 1 <= preorder.length <= 3000 
+// inorder.length == preorder.length 
+// -3000 <= preorder[i], inorder[i] <= 3000 
+// preorder 和 inorder 均 无重复 元素 
+// inorder 均出现在 preorder 
+// preorder 保证 为二叉树的前序遍历序列 
+// inorder 保证 为二叉树的中序遍历序列 
+// 
+//
+// Related Topics 树 数组 哈希表 分治 二叉树 👍 2622 👎 0
+
+package editor.cn;
+
+import javax.swing.tree.TreeNode;
+
+public class ConstructBinaryTreeFromPreorderAndInorderTraversal {
+    public static void main(String[] args) {
+        Solution solution = new ConstructBinaryTreeFromPreorderAndInorderTraversal().new Solution();
+        System.out.println("Hello world");
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    private int preIndex = 0;
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        preIndex = 0;
+        return buildTree(preorder, inorder, 0, inorder.length - 1);
+    }
+
+    private TreeNode buildTree(int[] preorder, int[] inorder, int start, int end) {
+        if(start > end) return null;
+
+        int rootVal = preorder[preIndex++];
+        TreeNode root = new TreeNode(rootVal);
+
+        int rootIndex = -1;
+        for (int i = start; i <= end; i++) {
+            if (inorder[i] == rootVal) {
+                rootIndex = i;
+                break;
+            }
+        }
+
+        root.left = buildTree(preorder, inorder, start, rootIndex - 1);
+        root.right = buildTree(preorder, inorder, rootIndex + 1, end);
+
+        return root;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+} 
