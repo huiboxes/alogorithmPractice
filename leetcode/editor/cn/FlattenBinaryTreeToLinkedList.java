025887b3318d333c27b7aa53c1d2e5553dc05314
kylo <huiboxes@gmail.com>
2025-11-16 21:38:08 +0800

    feat: 114-递归先序遍历。节点都存到数组中，然后遍历数组将节点左子树设为null，右子树设为下一个节点

---
+++ leetcode/editor/cn/FlattenBinaryTreeToLinkedList.java
@@ -0,0 +1,101 @@
+/**
+  * 题目Id：114
+  * 题目：二叉树展开为链表
+  * 日期：2025-11-16 21:16:29
+*/
+//给你二叉树的根结点 root ，请你将它展开为一个单链表： 
+//
+// 
+// 展开后的单链表应该同样使用 TreeNode ，其中 right 子指针指向链表中下一个结点，而左子指针始终为 null 。 
+// 展开后的单链表应该与二叉树 先序遍历 顺序相同。 
+// 
+//
+// 
+//
+// 示例 1： 
+// 
+// 
+//输入：root = [1,2,5,3,4,null,6]
+//输出：[1,null,2,null,3,null,4,null,5,null,6]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：root = []
+//输出：[]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：root = [0]
+//输出：[0]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中结点数在范围 [0, 2000] 内 
+// -100 <= Node.val <= 100 
+// 
+//
+// 
+//
+// 进阶：你可以使用原地算法（O(1) 额外空间）展开这棵树吗？ 
+//
+// Related Topics 栈 树 深度优先搜索 链表 二叉树 👍 1917 👎 0
+
+package editor.cn;
+
+import javax.swing.tree.TreeNode;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FlattenBinaryTreeToLinkedList {
+    public static void main(String[] args) {
+        Solution solution = new FlattenBinaryTreeToLinkedList().new Solution();
+        System.out.println("Hello world");
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public void flatten(TreeNode root) {
+        if(root == null) return;
+        ArrayList<TreeNode> list = new ArrayList<>();
+
+        preOrder(root, list);
+
+        for (int i = 0; i < list.size() - 1; i++) {
+            list.get(i).left = null;
+            list.get(i).right = list.get(i + 1);
+        }
+
+    }
+
+    private void preOrder(TreeNode node, List<TreeNode> list) {
+        if(node == null) return;
+        list.add(node);
+        preOrder(node.left, list);
+        preOrder(node.right, list);
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+} 
